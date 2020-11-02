09a9eee2fdac51dd3bcd31e97c5dee0f6675b7e7
BaSS <BaSS@DESKTOP-VK0DT3U>
2020-11-02 02:48:50 +0100

    Realizados varios cambios
    Añadido comentarios
    Añadido campo contraseña
    Arreglado metodo de cifrado de fichero
    Corregido error al no introducir contraseña

--- SRT_cifrador/src/Cypher.java
+++ SRT_cifrador/src/Cypher.java
@@ -1,21 +1,13 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -25,20 +17,7 @@ import utils.Header;
 import utils.Options;
 
 public class Cypher {
-	// "PBEWithMD5AndDES"; "PBEWithMD5AndDES";
-	/*
-	 * TODO Borrar este msj
-	 * 
-	 * Se necesitan 3 arrays de datos (formato byte)
-	 * 
-	 * 1- Mensaje (pasar de string a bytes) que se desea encriptar 2- Clave 3-
-	 * Vector de inicialización
-	 * 
-	 * Para desencriptar mas de lo mismo
-	 * 
-	 * 1- Contraseña 2- Salt 3- Entero con numero de iteraciones
-	 * 
-	 */
+	// TODO Borrar "PBEWithMD5AndDES"; "PBEWithMD5AndDES";
 
 	/*
 	 * TODO Borrar esto
@@ -51,153 +30,95 @@ public class Cypher {
 	 * Mover ese Spam de try/catch
 	 * 
 	 */
-
-	private PBEKeySpec pbeKeySpec;
-	private PBEParameterSpec pbeParam;
-
-	private SecretKeyFactory kFactory;
-	private SecretKey sKey;
-
-	private Cipher c;
-	private CipherOutputStream cOut;
+	
+	private int itCount;
 
 	public Cypher() {
 
-		try {
-			init();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchProviderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.itCount = 1024;
 	}
 
-	private void init() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException,
-			InvalidKeySpecException, InvalidKeyException, InvalidAlgorithmParameterException {
+	public void cipherFile(File file, String alg, String pw) throws Exception {
 
 		// Create password
-		char[] pass = "testeando".toCharArray();
-		pbeKeySpec = new PBEKeySpec(pass);
-
-		// Create byte salt
-		Random random = new Random();
-		byte[] saltBytes = new byte[32];
-		random.nextBytes(saltBytes);
-
-		// Create iteration count
-		int itCount = 1024;
-
-		pbeParam = new PBEParameterSpec(saltBytes, itCount);
-
-		kFactory = SecretKeyFactory.getInstance("PBEWithSHAAnd3KeyTripleDES", "BC");
-		sKey = kFactory.generateSecret(pbeKeySpec);
-
-		c = Cipher.getInstance("PBEWithSHAAnd3KeyTripleDES", "BC");
-		c.init(Cipher.ENCRYPT_MODE, sKey, pbeParam);
-	}
-
-	public static void cipherFile(File file, String alg, String pw) throws Exception {
-
-		int itCount = 1024;
-
-		// Create password
-		char[] pass = "testeando".toCharArray();
-		pbeKeySpec = new PBEKeySpec(pass);
+		char[] pass = pw.toCharArray();
+		PBEKeySpec pbeKeySpec = new PBEKeySpec(pass);
 
 		// Create byte salt
 		SecureRandom random = new SecureRandom();
-		byte[] saltBytes = new byte[32];
+		byte[] saltBytes = new byte[8];
 		random.nextBytes(saltBytes);
 
 		// Create header
-
 		Header header = new Header(Options.OP_SYMMETRIC_CIPHER, alg, "", saltBytes);
 
+		// Set options
 		PBEParameterSpec pPS = new PBEParameterSpec(header.getData(), itCount);
 
-		kFactory = SecretKeyFactory.getInstance(header.getAlgorithm1());
-		sKey = kFactory.generateSecret(pbeKeySpec);
+		// Generate session key based on algorythm
+		SecretKeyFactory kFactory = SecretKeyFactory.getInstance(header.getAlgorithm1());
+		SecretKey sKey = kFactory.generateSecret(pbeKeySpec);
 
-		c = Cipher.getInstance(alg);
-		c.init(Cipher.ENCRYPT_MODE, sKey, pbeParam);
+		// Set and initialize cypher using an algorythm, and also set the key and parameters 
+		Cipher c = Cipher.getInstance(alg);
+		c.init(Cipher.ENCRYPT_MODE, sKey, pPS);
 
-		// Flujos de ficheros
-		OutputStream ficheroSalida = new FileOutputStream(file + ".cif");
-		header.save(ficheroSalida);
+		// Get streams to operate on the file
+		OutputStream fileOUT = new FileOutputStream(file + ".cif"); // Add .cif sufix
+		header.save(fileOUT);
 		FileInputStream ficheroEntrada = new FileInputStream(file);
+		CipherOutputStream cOut = new CipherOutputStream(fileOUT, c);
 
-		CipherOutputStream cos = new CipherOutputStream(ficheroSalida, c);
-
-		// Accion de cifrado de fichero
-		int iterlec;
+		// Cypher file
+		int isEmpty; // If there are no more bits on the file, .read returns -1
 		byte[] block = new byte[16];
-		while ((iterlec = ficheroEntrada.read(block)) != -1) {
-			cos.write(block, 0, iterlec);
-			// System.out.println("encrypted: " + Hex.toHexString(block));
+		while ((isEmpty = ficheroEntrada.read(block)) != -1) {
+			cOut.write(block, 0, isEmpty);
+			// TODO BORRAR System.out.println("encrypted: " + Hex.toHexString(block));
 		}
-		cos.close();
+		cOut.close();
 		ficheroEntrada.close();
-		ficheroSalida.close();
-		System.out.println("El fichero se ha cifrado correctamente");
+		fileOUT.close();
 	}
 
-	public void decipherFile(File fichero, String password) throws Exception {
-		// Fichero de entrada
+	public void decipherFile(File fichero, String pw) throws Exception {
+		
+		// Get the stream of the IN file
 		InputStream ficheroEntrada = new FileInputStream(fichero);
 
-		// Cabecera
+		// Read header
 		Header header = new Header();
 		header.load(ficheroEntrada);
 
-		// Generacion de clave
-		char[] pass = "testeando".toCharArray();
-		int iterationCount = 1024;
+		// Create password
+		char[] pass = pw.toCharArray();
 
-		// SecrectKeyFactory: Nos permitirá generar la clave de sesión para el algoritmo
-		// elegido; la crearemos indicando el algoritmo para el que se quiere la clave
+		// Set options
 		PBEKeySpec pbeKeySpec = new PBEKeySpec(pass);
-		PBEParameterSpec pPS = new PBEParameterSpec(header.getData(), iterationCount);
+		PBEParameterSpec pPS = new PBEParameterSpec(header.getData(), itCount);
+		
+		// Generate session key based on algorythm
+		SecretKeyFactory kFactory = SecretKeyFactory.getInstance(header.getAlgorithm1());
+		SecretKey sKey = kFactory.generateSecret(pbeKeySpec);
 
-		kFactory = SecretKeyFactory.getInstance(header.getAlgorithm1());
-		sKey = kFactory.generateSecret(pbeKeySpec);
+		// Set and initialize cypher using an algorythm, and also set the key and parameters 
+		Cipher c = Cipher.getInstance(header.getAlgorithm1());
+		c.init(Cipher.ENCRYPT_MODE, sKey, pPS);
 
-		// Descifrado del fichero
-		// Cipher: Clase cifrador; la crearemos indicando el algoritmo y después la
-		// iniciaremos con la clave y los parámetros
-
-		c = Cipher.getInstance(header.getAlgorithm1());
-		c.init(Cipher.ENCRYPT_MODE, sKey, pbeParam);
-
-		// Accion de generacion de ficheros de salida
+		// Get streams to operate on the file
 		String nombre = fichero.getAbsolutePath();
-		nombre = nombre.substring(0, nombre.length() - 4);
+		nombre = nombre.substring(0, nombre.length() - 4); // Eliminate .cif sufix
 		FileOutputStream ficheroSalida = new FileOutputStream(nombre);
-		CipherInputStream cos = new CipherInputStream(ficheroEntrada, c);
+		CipherInputStream cIn = new CipherInputStream(ficheroEntrada, c);
 
+		// De-cypher file	
+		int isEmpty; // If there are no more bits on the file, .read returns -1
 		byte[] bloque = new byte[1024];
-		int iterlec;
-		while ((iterlec = cos.read(bloque)) != -1) {
-			ficheroSalida.write(bloque, 0, iterlec);
+		while ((isEmpty = cIn.read(bloque)) != -1) {
+			ficheroSalida.write(bloque, 0, isEmpty);
 		}
-
-		cos.close();
+		cIn.close();
 		ficheroEntrada.close();
 		ficheroSalida.close();
-		System.out.println("El fichero se ha descifrado correctamente");
-
 	}
 }
--- SRT_cifrador/src/Interface.java
+++ SRT_cifrador/src/Interface.java
@@ -1,7 +1,6 @@
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -11,8 +10,8 @@ import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -26,8 +25,10 @@ public class Interface extends JFrame {
 	 */
 	private static final long serialVersionUID = -6163772603012575756L;
 
-	private static final Dimension MIN_SIZE = new Dimension(320, 360);
-	private static final Dimension DEFAULT_SIZE = new Dimension(640, 500);
+	private static final Dimension MIN_SIZE = new Dimension(320, 330);
+	private static final Dimension DEFAULT_SIZE = new Dimension(640, 470);
+
+	Cypher cypher;
 
 	private JLabel rootLabel;
 	private JLabel algoryLabel;
@@ -36,7 +37,7 @@ public class Interface extends JFrame {
 	private JLabel statusLabel;
 	private JLabel pwLabel;
 	private JTextField rootTextField;
-	private JTextField passwordTextField;
+	private JPasswordField passwordField;
 	private JComboBox<String> algoryComboBox;
 	private JButton rootButton;
 	private JButton acceptButton;
@@ -46,7 +47,6 @@ public class Interface extends JFrame {
 	private JTextArea cipherFileArea;
 
 	private File rootPath;
-	private String password;
 
 	public Interface() {
 		initComponents();
@@ -54,9 +54,13 @@ public class Interface extends JFrame {
 		finishGui();
 	}
 
+	/*
+	 * Initiate GUI components
+	 */
 	private void initComponents() {
 
 		String[] algorythms = { "AES", "DES", "3DES" };
+		cypher = new Cypher();
 
 		rootLabel = new JLabel();
 		algoryLabel = new JLabel();
@@ -66,7 +70,7 @@ public class Interface extends JFrame {
 		pwLabel = new JLabel();
 
 		rootTextField = new JTextField();
-		passwordTextField = new JTextField();
+		passwordField = new JPasswordField();
 
 		algoryComboBox = new JComboBox<String>(algorythms);
 
@@ -84,7 +88,7 @@ public class Interface extends JFrame {
 		outFileLabel.setText("Resultado del cifrado");
 		rootButton.setText("…");
 		acceptButton.setText("Aceptar");
-		pwLabel.setText("Contraseña: ");
+		pwLabel.setText("Contraseña:");
 
 		rootTextField.setEditable(false);
 		algoryComboBox.setEditable(false);
@@ -92,6 +96,8 @@ public class Interface extends JFrame {
 		cipherFileArea.setEditable(false);
 		rootButton.setFocusable(false);
 		acceptButton.setFocusable(false);
+
+		passwordField.setEchoChar('*'); // Type * as the user writes in the component
 
 		rootButton.addActionListener(e -> {
 			try {
@@ -104,9 +110,14 @@ public class Interface extends JFrame {
 		acceptButton.addActionListener(this::startEncryption);
 	}
 
+	/*
+	 * Initiate the layout for the JFrame window
+	 */
 	private void initLayout() {
 		GroupLayout layout = new GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
+
+		// Horizontal groups
 		layout.setHorizontalGroup(layout.createSequentialGroup().addContainerGap()
 				.addGroup(layout.createParallelGroup()
 						.addGroup(layout.createSequentialGroup().addComponent(rootLabel)
@@ -116,18 +127,19 @@ public class Interface extends JFrame {
 								.addPreferredGap(ComponentPlacement.RELATED).addComponent(algoryComboBox)
 								.addPreferredGap(ComponentPlacement.RELATED).addComponent(acceptButton))
 						.addGroup(layout.createSequentialGroup().addComponent(pwLabel)
-								.addPreferredGap(ComponentPlacement.RELATED).addComponent(passwordTextField))
+								.addPreferredGap(ComponentPlacement.RELATED).addComponent(passwordField))
 						.addComponent(inFileLabel).addComponent(filePane).addComponent(outFileLabel)
 						.addComponent(resultsPane).addComponent(statusLabel))
 				.addContainerGap());
 
+		// Vertical groups
 		layout.setVerticalGroup(layout.createSequentialGroup().addContainerGap()
 				.addGroup(layout.createParallelGroup().addComponent(rootLabel).addComponent(rootTextField)
 						.addComponent(rootButton))
 				.addPreferredGap(ComponentPlacement.RELATED)
 				.addGroup(layout.createParallelGroup().addComponent(algoryLabel).addComponent(algoryComboBox))
 				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(layout.createParallelGroup().addComponent(pwLabel).addComponent(passwordTextField))
+				.addGroup(layout.createParallelGroup().addComponent(pwLabel).addComponent(passwordField))
 				.addPreferredGap(ComponentPlacement.RELATED).addComponent(inFileLabel)
 				.addPreferredGap(ComponentPlacement.RELATED).addComponent(filePane)
 				.addPreferredGap(ComponentPlacement.RELATED).addComponent(outFileLabel)
@@ -135,9 +147,13 @@ public class Interface extends JFrame {
 				.addPreferredGap(ComponentPlacement.RELATED).addComponent(acceptButton)
 				.addPreferredGap(ComponentPlacement.RELATED).addComponent(statusLabel).addContainerGap());
 
+		// Link size of labels
 		layout.linkSize(SwingConstants.HORIZONTAL, rootLabel, algoryLabel, pwLabel);
 	}
 
+	/*
+	 * Set the last parameters of the main window
+	 */
 	private void finishGui() {
 		pack();
 		setTitle("Trabajo SRT");
@@ -149,17 +165,24 @@ public class Interface extends JFrame {
 		updateStatus("Preparado para encriptar.");
 	}
 
+	/*
+	 * File chooser
+	 */
 	private void selectRoot(ActionEvent event) throws IOException {
 		JFileChooser chooser = new JFileChooser();
 		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-			statusLabel.setText("Fichero seleccionado.");
+			updateStatus("Fichero seleccionado.");
 			rootPath = chooser.getSelectedFile();
-			rootTextField.setText(rootPath.getAbsolutePath());
-			previewFile(rootPath.getAbsolutePath());
+			rootTextField.setText(rootPath.getAbsolutePath()); // Display file path once chosen
+			previewFile(rootPath.getAbsolutePath()); // Show file content on preview zone
 		}
 	}
 
+	/*
+	 * Method to read the characters from the IN file and display them in the
+	 * corresponding area
+	 */
 	private void previewFile(String fileRoot) throws IOException {
 
 		FileReader fileReader = new FileReader(fileRoot);
@@ -172,24 +195,83 @@ public class Interface extends JFrame {
 		fileReader.close();
 	}
 
+	/*
+	 * Method to read the characters from the OUTPUT file after the encryption and
+	 * display them in the corresponding area
+	 */
+	private void previewEncryption() throws IOException {
+
+		FileReader fileReader = new FileReader(rootPath.getAbsolutePath() + ".cif");
+		char[] display = new char[300];
+		fileReader.read(display, 0, 300);
+
+		cipherFileArea.setText(String.valueOf(display));
+
+		fileReader.close();
+	}
+
+	/*
+	 * Method to initiate the cyphering process once the "accept" button is clicked
+	 */
 	private void startEncryption(ActionEvent event) {
 
 		if (rootPath != null) {
-			if (passwordTextField.getText() != null) {
-				statusLabel.setText("Cifrando con algoritmo : " + algoryComboBox.getSelectedItem().toString());
-				Cypher.cipherFile(rootPath, algoryComboBox.getSelectedItem().toString(), passwordTextField.getText());
-				// algoryComboBox.getSelectedIndex();
-				statusLabel.setText("Fichero cifrado correctamente. ");
-				//TODO: mostrar mensaje de fichero cifrado correctamente ventana emergente con boton aceptar
-				//TODO: menu elegir cifrar descifrar
+			if (passwordField.getPassword().length != 0) {
+				updateStatus("Cifrando con algoritmo : " + algoryComboBox.getSelectedItem().toString());
+
+				try {
+					cypher.cipherFile(rootPath, parseAlgoChosen(algoryComboBox.getSelectedIndex()),
+							String.valueOf(passwordField.getPassword()));
+					previewEncryption();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				statusLabel.setText("Fichero cifrado correctamente.");
+				// TODO: mostrar mensaje de fichero cifrado correctamente ventana emergente con
+				// boton aceptar
+				// TODO: menu elegir cifrar descifrar
 			} else {
-				statusLabel.setText("ERROR : No se ha insertado ninguna contraseña. ");
+				updateStatus("ERROR : No se ha insertado ninguna contraseña.");
 			}
 		} else {
-			statusLabel.setText("ERROR : No se ha seleccionado ningún fichero. ");
+			updateStatus("ERROR : No se ha seleccionado ningún fichero.");
 		}
 	}
 
+	/*
+	 * Method to translate the drop down list into the exact algorithm name for
+	 * later calls
+	 */
+	private String parseAlgoChosen(int op) {
+
+		String chosen;
+
+		switch (op) {
+
+		case 0:
+			chosen = "PBEWithMD5AndAES";
+			break;
+
+		case 1:
+			chosen = "PBEWithMD5AndDES";
+			break;
+
+		case 2:
+			chosen = "PBEWithMD5AndDESede";
+			break;
+
+		default:
+			chosen = "PBEWithMD5AndAES";
+			break;
+		}
+
+		return chosen;
+	}
+
+	/*
+	 * Update the status label at the bottom
+	 */
 	private void updateStatus(String status, Object... args) {
 		String formatted = String.format(status, args);
 		statusLabel.setText(formatted);
@@ -197,7 +279,7 @@ public class Interface extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		java.awt.EventQueue.invokeLater(Interface::new);
+		java.awt.EventQueue.invokeLater(Interface::new); // Call EventQueue on the UI to avoid freezes
 	}
 
 }
