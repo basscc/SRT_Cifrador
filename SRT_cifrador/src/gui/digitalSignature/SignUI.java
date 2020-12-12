e0070f64313b7b70f4a22ca6115cb871621199fe
BaSS <BaSS@DESKTOP-VK0DT3U>
2020-12-12 18:00:37 +0100

    Preparada la interfaz de SignUI

--- SRT_cifrador/src/gui/digitalSignature/SignUI.java
+++ SRT_cifrador/src/gui/digitalSignature/SignUI.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -38,8 +39,8 @@ public class SignUI extends JFrame {
 	 */
 	private static final long serialVersionUID = 5723206829220885984L;
 
-	private static final Dimension MIN_SIZE = new Dimension(300, 250);
-	private static final Dimension DEFAULT_SIZE = new Dimension(500, 300);
+	private static final Dimension MIN_SIZE = new Dimension(400, 140);
+	private static final Dimension DEFAULT_SIZE = new Dimension(500, 180);
 
 	FirmaDigitalUI parentUI;
 	DigitalSignature ds;
@@ -47,8 +48,10 @@ public class SignUI extends JFrame {
 	private Boolean opSuccessfull; // bool to determine if an operation was sucessfully executed
 
 	private JLabel rootLabel;
+	private JLabel signLabel;
 	private JLabel statusLabel;
 	private JTextField rootTextField;
+	private JComboBox<String> signComboBox;
 	private JButton rootButton;
 	private JButton acceptButton;
 	private JButton backButton;
@@ -68,19 +71,24 @@ public class SignUI extends JFrame {
 	private void initComponents() {
 
 		ds = new DigitalSignature();
+		String[] signatures = { "Prueba 1", "Prueba 2" };
 
 		opSuccessfull = false;
 
 		rootLabel = new JLabel();
+		signLabel = new JLabel();
 		statusLabel = new JLabel();
 
 		rootTextField = new JTextField();
+		
+		signComboBox = new JComboBox<String>(signatures);
 
 		rootButton = new JButton();
 		acceptButton = new JButton();
 		backButton = new JButton();
 
 		rootLabel.setText("Ruta de fichero:");
+		signLabel.setText("Eliga la firma:" );
 		rootButton.setText("…");
 		acceptButton.setText("Firmar");
 		backButton.setText("Volver");
@@ -115,9 +123,11 @@ public class SignUI extends JFrame {
 				.addGroup(layout.createParallelGroup()
 						.addGroup(layout.createSequentialGroup().addComponent(rootLabel)
 								.addPreferredGap(ComponentPlacement.RELATED).addComponent(rootTextField)
-								.addPreferredGap(ComponentPlacement.RELATED).addComponent(rootButton)
-								.addPreferredGap(ComponentPlacement.RELATED).addComponent(acceptButton)
-								.addPreferredGap(ComponentPlacement.RELATED).addComponent(backButton))
+								.addPreferredGap(ComponentPlacement.RELATED).addComponent(rootButton))
+						.addGroup(layout.createSequentialGroup().addComponent(signLabel)
+								.addPreferredGap(ComponentPlacement.RELATED).addComponent(signComboBox)
+								.addPreferredGap(ComponentPlacement.RELATED).addComponent(acceptButton))
+						.addComponent(backButton)
 						.addComponent(statusLabel))
 				.addContainerGap());
 
@@ -125,12 +135,16 @@ public class SignUI extends JFrame {
 		layout.setVerticalGroup(layout.createSequentialGroup().addContainerGap()
 				.addGroup(layout.createParallelGroup().addComponent(rootLabel).addComponent(rootTextField)
 						.addComponent(rootButton))
-				.addPreferredGap(ComponentPlacement.RELATED).addPreferredGap(ComponentPlacement.RELATED)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addGroup(layout.createParallelGroup().addComponent(signLabel).addComponent(signComboBox))
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addPreferredGap(ComponentPlacement.RELATED)
 				.addGroup(layout.createParallelGroup().addComponent(acceptButton).addComponent(backButton))
+				.addPreferredGap(ComponentPlacement.RELATED).addPreferredGap(ComponentPlacement.RELATED)
 				.addComponent(statusLabel).addContainerGap());
 
 		// Link size of labels
-		layout.linkSize(SwingConstants.HORIZONTAL, rootLabel);
+		layout.linkSize(SwingConstants.HORIZONTAL, rootLabel, signLabel);
 	}
 
 	/*
@@ -178,6 +192,32 @@ public class SignUI extends JFrame {
 		if (rootPath != null) {
 
 		}
+	}
+	
+	/* TODO
+	 * Method to translate the drop down list into the exact algorithm name for
+	 * later calls
+	 */
+	private String parseHashChosen(int op) {
+
+		String chosen;
+
+		switch (op) {
+
+		case 0:
+			chosen = "aa";
+			break;
+
+		case 1:
+			chosen = "bb";
+			break;
+			
+		default:
+			chosen = "unodefault";
+			break;
+		}
+
+		return chosen;
 	}
 
 	/*
